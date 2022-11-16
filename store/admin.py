c777c3fa2c3f3b0fd85e8fc04303efbddd34a041
adrian <adrian.najmrodzki@gmail.com>
2022-11-16 18:21:58 +0100

    16.11.2022 18.21

--- store/admin.py
+++ store/admin.py
@@ -5,18 +5,18 @@ from store.models import Product, Order, UnitOrder, TypeProduct, SubTypeProduct
 
 admin.site.register(TypeProduct)
 
-admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(UnitOrder)
 
 
 
-@admin.register(SubTypeProduct)
-class ProfileAdmin(ModelAdmin):
-    ordering = ['id']
-    list_display = ['id', 'type_product', 'sub_type']
-    # list_display_links = ['id', 'name']
-    # list_per_page = 20
+
+
+
+
+
+
+
 
 
 
