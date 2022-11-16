5ae8edb07d23b6a38ceaa71f1ff1c8261d7172d0
donut-panic <yaroslawek@gmail.com>
2022-11-16 20:24:00 +0100

    Fixed models

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
-from django.contrib.admin import ModelAdmin
 
-from store.models import Product, Order, UnitOrder, TypeProduct, SubTypeProduct
+from store.models import Product, Order, UnitOrder, Category, ShippingMethod, PaymentMethod, OrderStatus
 
-admin.site.register(TypeProduct)
-
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ShippingMethod)
+admin.site.register(PaymentMethod)
+admin.site.register(OrderStatus)
 admin.site.register(Order)
 admin.site.register(UnitOrder)
+
 
 
 
