7bf895a09801553e95a6573c4a65301d46f2bf60
AdrianNajmrodzki <adrian.najmrodzki@gmail.com>
2022-11-18 15:06:35 +0100

    Search working ! Simple version, only finds what exactly you are looking for.

--- store/views.py
+++ store/views.py
@@ -1,6 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import TemplateView, DetailView
 
